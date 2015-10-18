475ca51a060c739420f7bd94452136839560288b
prabhushan <priyan.prabhu@gmail.com>
2015-10-17 23:12:28 -0400

    changes

--- src/main/java/mongodb/Main.java
+++ src/main/java/mongodb/Main.java
@@ -1,9 +1,22 @@
 package mongodb;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello Sai");
+		Spark.get(new Route("/"){
+
+			@Override
+			public Object handle(Request arg0, Response arg1) {
+				
+				return "Sai Ram";
+			}
+			
+		});
 
 	}
 
